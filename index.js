7f9a34baf0af3291445068c41891b4863ac37154
deltaprojectfit <deltaprojectfit.dev@gmail.com>
2023-12-10 14:30:37 -0800

    netlify deploy

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const path = require('path');
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 // Use EJS
 app.set('view engine', 'ejs');
